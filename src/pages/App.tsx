963e54fac39ad94dc3f6665cc30b76d74ddff5e9
1t1sCooL <mmalabugin@gmail.com>
2025-08-03 21:15:14 +0300

    update styles and adaptive design

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -20,10 +20,10 @@ return(<>
                 Our Equilibrium collection promotes balance and calm.
             </p>
             <div className={styles.flexWrap}>
-                <div className={styles.priceWrap}><svg width="11" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z" fill="#00FFF8"/>
+                <div className={styles.priceWrap}><svg width="11" height="18" viewBox="0 0 11 18" xmlns="http://www.w3.org/2000/svg"><path d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z" fill="#00FFF8"/>
                 </svg><span className={styles.price}>0.041 ETH</span></div>
                 <div className={styles.daysLeftWrap}>
-                    <svg width="17" height="17" xmlns="http://www.w3.org/2000/svg"><path d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z" fill="#8BACD9"/>
+                    <svg width="17" height="17" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg"><path d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z" fill="#8BACD9"/>
                     </svg> <span className={styles.daysLeft}>3 days left</span>
                 </div>
             </div>
