fd00753bd81cfadab9a90eeced8dca10a4585336
1t1sCooL <mmalabugin@gmail.com>
2025-12-31 06:00:44 +0300

    CI/CD

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,13 +1,17 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-const Footer = () =>{
-    return (
-        <footer>
-            <div className={styles.footer}>
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-                Coded by <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>.
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer>
+      <div className={styles.footer}>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>.
+      </div>
+    </footer>
+  );
+};
 export default Footer;
